00ddc791e7b82ad1c0bdba1ba60c44c742b9f193
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-08 15:06:04 +0200

    add HeaderBackground component and styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,26 +3,30 @@ import Image from "next/image";
 
 import Logo from "@/assets/logo.png";
 import styles from "./Header.module.css";
+import HeaderBackground from "./HeaderBackground";
 
 function Header() {
   return (
-    <header className={styles.header}>
-      <Link href="/" className={styles.logo}>
-        <Image src={Logo} alt="Logo" priority />
-        Foodies
-      </Link>
+    <>
+      <HeaderBackground />
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          <Image src={Logo} alt="Logo" priority />
+          Foodies
+        </Link>
 
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
 
