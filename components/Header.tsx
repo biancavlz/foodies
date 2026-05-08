da71fb18f0bc5ee19a63048d806a074c8cea783b
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-08 14:30:48 +0200

    Styles main Header with css modules

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import Logo from "@/assets/logo.png";
+import styles from "./Header.module.css";
 
 function Header() {
   return (
-    <header>
-      <Link href="/">
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
         <img src={Logo.src} alt="Logo" />
         Foodies
       </Link>
 
-      <nav>
+      <nav className={styles.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse meals</Link>
