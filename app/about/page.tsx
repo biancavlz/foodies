a25862525bdc51ba2d52c65d8a02e97b7c1e3c91
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-08 10:57:05 +0200

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <main>
+      <div>About us </div>
+    </main>
+  );
+}
+
+export default Page;
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <>
+      <img src="/logo.png" alt="logo" />
+      <h1>Welcome</h1>
+    </>
+  );
+}
+
+export default Header;
