9563b017cf5e2503f78f62fdc2386e5dcd146f40
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-11 09:49:54 +0200

    Add styling to the meals page

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
 function Home() {
-  return <h1>Meals</h1>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals created by{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cooki ti yourself. It is easy and fun
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}></main>;
+    </>
+  );
 }
 
 export default Home;
