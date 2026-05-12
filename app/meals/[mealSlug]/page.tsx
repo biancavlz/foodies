781f550de13ed59e9af49f5d2e5aa74c8079644b
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-12 13:39:22 +0200

    fetch meal details data and apply

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,5 +1,42 @@
-function Home() {
-  return <h1>Meal Details</h1>;
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+type MealDetailsProps = {
+  params: {
+    mealSlug: string;
+  };
+};
+
+async function MealsDetails({ params }: MealDetailsProps) {
+  const { mealSlug } = params;
+  const meal = await getMeal(mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by
+            <a href={`mailto: ${`meal.creator_email`} `}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        />
+      </main>
+    </>
+  );
 }
 
-export default Home;
+export default MealsDetails;
