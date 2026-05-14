9ec225f8183d8715ed99ad0b8c41b84d25931516
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-14 12:53:48 +0200

    Add ImagePicker preview

--- components/meals/ImagePicker.tsx
+++ components/meals/ImagePicker.tsx
@@ -1,19 +1,47 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import styles from "./ImagePicker.module.css";
 import { MealImagePicker } from "@/types/meal";
+import Image from "next/image";
 
 function ImagePicker({ label, name }: MealImagePicker) {
+  const [pickImage, setPickImage] = useState<string | null>(null);
   const imageInputRef = useRef<HTMLInputElement | null>(null);
 
   function handleClick() {
     imageInputRef.current?.click();
   }
+
+  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      setPickImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      if (typeof fileReader.result === "string") {
+        setPickImage(fileReader.result);
+      }
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={styles.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={styles.controls}>
+        <div className={styles.preview}>
+          {!pickImage && <p>No Image picked yet.</p>}
+          {pickImage && (
+            <Image src={pickImage} alt="Image selected by the user" fill />
+          )}
+        </div>
         <input
           className={styles.input}
           type="file"
@@ -21,6 +49,8 @@ function ImagePicker({ label, name }: MealImagePicker) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInputRef}
+          onChange={handleImageChange}
+          required
         />
         <button className={styles.button} type="button" onClick={handleClick}>
           Pick an image
